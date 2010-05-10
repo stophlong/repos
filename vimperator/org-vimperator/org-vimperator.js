fdbd3769de79e4b58516050446064230b95bcf84
Ryo Takaishi <s06t242@stmail.eng.kagawa-u.ac.jp>
2010-05-11 07:46:52 +0900

    org-protocolをvimperatorから操作するorg-vimperatorを追加

---
+++ vimperator/org-vimperator/org-vimperator.js
@@ -0,0 +1,98 @@
+let PLUGIN_INFO =
+<VimperatorPlugin>
+<name>{NAME}</name>
+<description>org-protocol interface for vimperator</description>
+<description lang="ja">org-protocolインタフェース</description>
+<author>Ryo Takaishi</author>
+<version>0.1</version>
+<minVersion>2.3.1</minVersion>
+<maxVersion>2.3.1</maxVersion>
+<updateURL>http://svn.coderepos.org/share/lang/javascript/vimperator-plugins/trunk/tombloo.js</updateURL>
+<detail><![CDATA[
+
+== COMMANDS ==
+
+:orgprotocol arg:
+    connect to emacsclient by sub-protocol(arg)
+    
+]]></detail>
+<detail lang="ja"><![CDATA[
+== COMMANDS ==
+
+:orgprotocol arg:
+    sub-protocol(arg)でemacsclientに接続します
+
+]]></detail>
+</VimperatorPlugin>;
+
+
+(function() {
+
+    function orgProtocolSendURL(arg) {
+        if (arg == "store-link") {
+            var url = "store-link://" + encodeURIComponent( window.content.document.URL) 
+                + "/" + encodeURIComponent( window.content.document.title);
+        } else if (arg == "remember") {
+            rememberTemplate = "";
+            var url = "remember://" + rememberTemplate 
+                + encodeURIComponent(window.content.document.URL) 
+                + "/" + encodeURIComponent(window.content.document.title) 
+                + "/" + encodeURIComponent(window.getSelection());
+        } else {
+            var url = ""
+        }
+
+        pref = Components.classes["@mozilla.org/preferences-service;1"]
+            .getService(Components.interfaces.nsIPrefBranch);
+        if( pref.getBoolPref("extensions.vimporg.macWorkaround") ) { // Workaround
+            var tmpFileName = "~/.org-fireforg-mac.tmp"
+            
+            var file = Components.classes["@mozilla.org/file/local;1"]
+            .createInstance(Components.interfaces.nsILocalFile);
+            file.initWithPath( tmpFileName );
+            if(file.exists() == false) {
+                file.create( Components.interfaces.nsIFile.NORMAL_FILE_TYPE, 420);
+            }
+
+            var stream = Components.classes["@mozilla.org/network/file-output-stream;1"]
+            .createInstance(Components.interfaces.nsIFileOutputStream);
+
+            stream.init(file, 0x02 | 0x08 | 0x10, 0666, 0);
+            var finalString = "org-protocol://" + url + "\n";
+            stream.write( finalString, finalString.length);
+            stream.close();
+
+        } else {
+            var req = new XMLHttpRequest();
+            try {
+                req.open('POST', "org-protocol://" + url,true);
+                req.send(null);
+            } catch (ex) { }
+        }
+    }
+
+    function hoge(){
+        liberator.echo("manageraaaa");
+    }
+
+    commands.addUserCommand(
+        ["orgprotocol"],
+        "org-protocol",
+        function(arg){
+            orgProtocolSendURL(arg);
+        },
+        {
+            completer : function (context, args) {
+                context.ancor = false;
+                context.ignoreCase = true;
+                context.title = ["url", "title"];
+                context.completions = [
+                    ["store-link", "store-link"],
+                    ["remember", "remember"],
+                ];
+            },
+        },
+        true      
+    );
+    
+})();
